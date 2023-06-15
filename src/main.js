5a2b1f34a734cad730d3f996b3b3e0f58f8a3032
BielovaIryna <belova83@gmail.com>
2023-06-15 10:24:10 +0300

    settings

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './js/by-now-modal';
+import './js/explore-chocolate-modal';
+import './js/leave-a-review-modal';
+import './js/mobile-menu';
+import './js/our-products-slider';
+import './js/reviews-slider';
